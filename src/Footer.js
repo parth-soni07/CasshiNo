316dc053b925e1d9c736f260f8bf95f6239d6189
Yukta Kumawat <yuktakumawat@gmail.com>
2024-07-05 13:13:08 +0530

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Footer(){
+    return(
+        <footer>
+            <p>&copy; 2024 our website. All rights reserved.</p>
+        </footer>
+    );
+}
+export default Footer;
---
+++ src/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Main()
+{
+    return (
+        <main>
+            <h2>About us</h2>
+            <p>This is the main content of the website</p>
+        </main>
+    );
+}
+export default Main;
